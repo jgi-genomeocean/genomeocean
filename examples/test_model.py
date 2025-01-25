a61f61eef528d3c3d8993105b581d6650accd3bf
fengchenLBL <63673457+fengchenLBL@users.noreply.github.com>
2025-01-24 16:51:08 -0800

    Create test_model.py
    script to test the docker

---
+++ examples/test_model.py
@@ -0,0 +1,54 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+# Step 1: Load the tokenizer
+tokenizer = AutoTokenizer.from_pretrained(
+    "pGenomeOcean/GenomeOcean-4B",
+    trust_remote_code=True,
+    padding_side="left",
+)
+
+# Step 2: Load the model
+model = AutoModelForCausalLM.from_pretrained(
+    "pGenomeOcean/GenomeOcean-4B",
+    trust_remote_code=True,
+    torch_dtype=torch.bfloat16,
+    attn_implementation="flash_attention_2",
+).to("cuda")
+
+print("Model and tokenizer downloaded successfully!")
+
+# Step 3: Embedding sequences
+sequences = [
+    "GCCGCTAAAAAGCGACCAGAATGATCCAAAAAAGAAGGCAGGCCAGCACCATCCGTTTTTTACAGCTCCAGAACTTCCTTT", 
+    "CAGTCAGTGGCTAGCATGCTAGCATCGATCGATCGATCGATCGATCGATCGATCGGTGCATGCTAGCATCGATCGATCGAA"
+]
+output = tokenizer.batch_encode_plus(
+    sequences,
+    max_length=10240,
+    return_tensors="pt",
+    padding="longest",
+    truncation=True
+)
+input_ids = output["input_ids"].cuda()
+attention_mask = output["attention_mask"].cuda()
+model_output = model.forward(input_ids=input_ids, attention_mask=attention_mask)[0].detach().cpu()
+attention_mask = attention_mask.unsqueeze(-1).detach().cpu()
+embedding = torch.sum(model_output * attention_mask, dim=1) / torch.sum(attention_mask, dim=1)
+print(f"Embedding Shape: {embedding.shape}")  # Should be (2, 3072)
+
+# Step 4: Sequence generation
+sequence = "GCCGCTAAAAAGCGACCAGAATGATCCAAAAAAGAAGGCAGGCCAGCACCATCCGTTTTTTACAGCTCCAGAACTTCCTTT"
+input_ids = tokenizer(sequence, return_tensors="pt", padding=True)["input_ids"]
+input_ids = input_ids[:, :-1].to("cuda")  # Remove the [SEP] token at the end
+model_output = model.generate(
+    input_ids=input_ids,
+    min_new_tokens=10,
+    max_new_tokens=10,
+    do_sample=True,
+    top_p=0.9,
+    temperature=1.0,
+    num_return_sequences=1,
+)
+generated = tokenizer.decode(model_output[0]).replace(" ", "")[5+len(sequence):]
+print(f"Generated sequence: {generated}")
